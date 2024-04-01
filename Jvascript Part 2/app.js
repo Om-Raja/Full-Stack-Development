fa539941aac8150754340e50235272a65830797d
Om Raja <omraja451@gmail.com>
2024-04-01 18:11:39 +0530

    solved question using switch case in jS

--- Jvascript Part 2/app.js
+++ Jvascript Part 2/app.js
@@ -66,3 +66,41 @@ else{
     console.log("It's not a good string");
 }
 
+let num2 = 0;
+if(num2)
+{
+    console.log("It's not zero");
+}
+else{
+    console.log("It is zero");
+}
+
+// using switch case to print day of the week
+
+let day = 7;
+switch(day)
+{
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3: 
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5: 
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7: 
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid input");
+}
