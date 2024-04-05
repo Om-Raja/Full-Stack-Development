783e0f532457250b325131bd2a00e1f51fa7d8d7
Om Raja <omraja451@gmail.com>
2024-04-05 21:30:48 +0530

    learnt js methods .trim(), indexOf()

--- Jvascript Part 2/app.js
+++ Jvascript Part 2/app.js
@@ -1,117 +1,128 @@
-console.log("Hey you!");
-console.log("Whar are you doing ?");
-console.log("all good ?");
-console.log("great!");
-let pencilPrice = 10;
-let scalePrice = 12;
-let result = "Total price is : " + pencilPrice + scalePrice + " rupees";
-console.log(result);
-result = "Total price is : " + (pencilPrice + scalePrice) + " rupees";
-console.log(result);
-console.log("Total price is :", pencilPrice + scalePrice , "rupees");
-console.log(`total price is ${pencilPrice + scalePrice} rupees`);
-console.log(`"tttttotal price will be ${pencilPrice + scalePrice} rupees"`);
-console.log(1 == "1");
-console.log(1 === "1");
-console.log(1 === 1);  
+// console.log("Hey you!");
+// console.log("Whar are you doing ?");
+// console.log("all good ?");
+// console.log("great!");
+// let pencilPrice = 10;
+// let scalePrice = 12;
+// let result = "Total price is : " + pencilPrice + scalePrice + " rupees";
+// console.log(result);
+// result = "Total price is : " + (pencilPrice + scalePrice) + " rupees";
+// console.log(result);
+// console.log("Total price is :", pencilPrice + scalePrice , "rupees");
+// console.log(`total price is ${pencilPrice + scalePrice} rupees`);
+// console.log(`"tttttotal price will be ${pencilPrice + scalePrice} rupees"`);
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log(1 === 1);  
 
-// practice question -> traffic light
+// // practice question -> traffic light
 
-let color ="yellow";
-switch (color) {
-    case "red":
-        console.log("Stop");
-        break;
-    case "yellow":
-        console.log("Slow down");
-        break;
-    case "green":
-        console.log("Go");
-        break;
-    default:
-        console.log("invalid color");
-}
+// let color ="yellow";
+// switch (color) {
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "yellow":
+//         console.log("Slow down");
+//         break;
+//     case "green":
+//         console.log("Go");
+//         break;
+//     default:
+//         console.log("invalid color");
+// }
 
-// popcorn price using if else
-let size = "K";
-if (size === "XL")
-{
-    console.log("Price is 250");
+// // popcorn price using if else
+// let size = "K";
+// if (size === "XL")
+// {
+//     console.log("Price is 250");
 
-}
-else if(size === "L")
-{
-    console.log("Price is 200");
-}
-else if(size === "M"){
-    console.log("Price is 100");
+// }
+// else if(size === "L")
+// {
+//     console.log("Price is 200");
+// }
+// else if(size === "M"){
+//     console.log("Price is 100");
 
-}
-else if(size ==="S")
-{
-    console.log("Price is 50");
-}
-else{
-    console.log("OOPs! This size is not available.");
-}
+// }
+// else if(size ==="S")
+// {
+//     console.log("Price is 50");
+// }
+// else{
+//     console.log("OOPs! This size is not available.");
+// }
 
-// finding good string using if else and logical operator
+// // finding good string using if else and logical operator
 
-let str = "aadar";
-if((str[0] === "a") && (str.length >  3))
-{
-    console.log("It's a good string");
-}
-else{
-    console.log("It's not a good string");
-}
+// let str = "aadar";
+// if((str[0] === "a") && (str.length >  3))
+// {
+//     console.log("It's a good string");
+// }
+// else{
+//     console.log("It's not a good string");
+// }
 
-let num2 = 0;
-if(num2)
-{
-    console.log("It's not zero");
-}
-else{
-    console.log("It is zero");
-}
+// let num2 = 0;
+// if(num2)
+// {
+//     console.log("It's not zero");
+// }
+// else{
+//     console.log("It is zero");
+// }
 
-// using switch case to print day of the week
+// // using switch case to print day of the week
 
-let day = 7;
-switch(day)
-{
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3: 
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5: 
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7: 
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Invalid input");
-}
+// let day = 7;
+// switch(day)
+// {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3: 
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5: 
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7: 
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid input");
+// }
 
-// alerts and prompt
-let YourRollNo = prompt("Enter your roll no.");
-alert("Something is wrong");
-console.log("roll no. = ", YourRollNo);
+// // alerts and prompt
+// let YourRollNo = prompt("Enter your roll no.");
+// alert("Something is wrong");
+// console.log("roll no. = ", YourRollNo);
 
-let firstName = prompt("Eneter your first name");
-let lastName = prompt("Enter your last name");
-console.log("Hi", firstName, lastName);
-let msg = "hi " + firstName + " " + lastName;
-alert(msg);
+// let firstName = prompt("Eneter your first name");
+// let lastName = prompt("Enter your last name");
+// console.log("Hi", firstName, lastName);
+// let msg = "hi " + firstName + " " + lastName;
+// alert(msg);
+
+// methods in JS
+let stringg = "    I love my papa   ";
+console.log(stringg);
+console.log(stringg.trim());
+let indexx = stringg.indexOf("papa");
+console.log(indexx);
+console.log(stringg.indexOf("papa"));
+console.log(stringg.indexOf("t"));
+console.log(stringg.indexOf("love"));
+console.log((stringg.trim()).indexOf("papa"));
