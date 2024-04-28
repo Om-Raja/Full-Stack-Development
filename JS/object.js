df0d4e7b79f587161c6a5bd0902923a45cba175b
Om Raja <omraja451@gmail.com>
2024-04-28 17:35:10 +0530

    learnt another metod to declare an object in js

--- JS/object.js
+++ JS/object.js
@@ -7,7 +7,7 @@ let student = {
     isHosteler: true
     // info are seperated by comma
     // it can contain all type of information such as string , number and boolean etc.
-}
+};
 
 // accessing object info
 console.log(student.Name);
@@ -18,3 +18,19 @@ console.log(CRN);
 console.log(student.URN);
 console.log(student.state);
 console.log(`is he hosteler : ${student.isHosteler}`);
+
+// another way of declaring an object
+var student2 = new Object();
+student2 = {
+    Name: "Nikita",
+    crn: 2217050,
+    urn: 2203739,
+    state: "Bihar",
+    isHosteller: false
+};
+
+console.log(student2.Name);
+console.log(student2.crn);
+console.log(student2.urn);
+console.log(student2.state);
+console.log(`is she hosteller : ${student2.isHosteller}`);
