eeb5b5ec2f468bda5737830832c079891f19c299
Om Raja <omraja451@gmail.com>
2024-04-28 16:19:59 +0530

    learnt forEach method

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// how to loop an array 
+// using forEach Method
+var arr = [1, 2, 3, 4];
+arr.forEach(function(value){
+    console.log(10 + value);
+});
+
+arr.forEach(function(hello){
+    console.log((20 + hello)) ;
+});
+
+var friends = ["Om", "Nikita", "Kashish", "Shikha", "Devendra"];
+friends.forEach(function(names){
+    console.log(names);
+});
