bafa24addc5202617c712b58ecc1df04a5b95eb0
Om Raja <omraja451@gmail.com>
2024-05-01 16:50:52 +0530

    learnt constructor function in js

---
+++ JS/constructorFunctionAndProtypalInheritance.js
@@ -0,0 +1,15 @@
+// It is good practice to name the constructor function with captital letter first
+function Constructor(name, age, crn, branch){
+    this.name = name; // In the context of the constructor function, "this" refers to new object that is being created.
+    this.age = age;
+    this.crn = crn;
+    this.branch = branch;
+}
+ 
+var Om = new Constructor("Om", 21, 2215197, "CSE"); // "new" is used to create new instances of the 'Constructor' object
+var Niki = new Constructor("Nikita", 19, 2217050, "ECE");
+
+console.log(Niki.crn);
+console.log(Om.branch);
+
+console.log(Niki);
