ee11a0ed2474368a38fb262ab6f2840ea5da8075
Om Raja <omraja451@gmail.com>
2024-05-02 14:14:15 +0530

    learnt about Math objects in js

---
+++ Day-18-Objects/mathObject.js
@@ -0,0 +1,21 @@
+// math object
+// Math.PI
+console.log(Math.PI);
+
+//Math.E
+console.log(Math.E);
+
+//Math.pow(a,b)
+console.log(Math.pow(2,4)); // 16
+
+//Math.floor()
+console.log(Math.floor(21.9)); // 21
+
+//Math.ceil()
+console.log(Math.ceil(21.1)); // 22
+
+//Math.random()
+console.log(Math.random()); // random number between 0 and 1(excluded)
+
+//Math.abs(n)
+console.log(Math.abs(-14)); // 14
