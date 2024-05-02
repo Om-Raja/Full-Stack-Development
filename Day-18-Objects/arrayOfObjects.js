f4c2b4a780edc728626495e8f573a727f19644ee
Om Raja <omraja451@gmail.com>
2024-05-02 14:06:27 +0530

    learnt array of objects

---
+++ Day-18-Objects/arrayOfObjects.js
@@ -0,0 +1,15 @@
+//array of objects
+let arr = [
+    {
+        name: "Om Raja",
+        branch: "CSE"
+    },
+
+    {
+        name: "Nikita",
+        branch: "ECE"
+    }
+];
+console.log(arr); //[{...},{...}]
+console.log(arr[1]); // {name: Nikita, branch: ECE}
+console.log(arr[0].name); // Om Raja
