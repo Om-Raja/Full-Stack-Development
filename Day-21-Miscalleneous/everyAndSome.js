5c222dd7df959ebde23253f59a6e3eae368df6d1
Om Raja <omraja451@gmail.com>
2024-05-05 00:05:04 +0530

    solved wrong output of odd checking function

--- Day-21-Miscalleneous/everyAndSome.js
+++ Day-21-Miscalleneous/everyAndSome.js
@@ -5,10 +5,10 @@ console.log(isEven); // true
 
 let array2 = [1, 3, 5, 7];
 let array3 = [3, 7, 9];
-let isOdd = array.every(n => { (n%2 == 0)});
+let isOdd = array3.every((n)=>{ 
+    return (n%2 != 0)});
 console.log(isOdd) // true
-
-
-console.log(array3.every((element) => {
-    (element % 2 != 0);
-}));
+let isOdd2 = array2.every((n) => {
+    return (n % 2 != 0);
+});
+console.log(isOdd2);
