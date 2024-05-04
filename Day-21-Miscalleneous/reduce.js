1474f9b6fc2bc8f3285016dd02e6f855f5ac56a8
Om Raja <omraja451@gmail.com>
2024-05-05 01:06:34 +0530

    learnt reduce method of array in js

---
+++ Day-21-Miscalleneous/reduce.js
@@ -0,0 +1,18 @@
+// reduce method of array in js
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = arr.reduce(function(accumulator, ele){
+    return (accumulator + ele);
+});
+// the above method will be called as many times as number of elements in the array.
+// accumulator will retain it's value from previous call of the method.
+
+console.log(sum); // output = 55 ✅
+
+let accumulator2 = 1000;
+
+let diff = arr.reduce((accumulator2, ele)=>{
+    return (accumulator2 - ele);
+});
+
+console.log(diff); // expected output : 1000 - 55 = 945, but we get (-53); Why ?? 
