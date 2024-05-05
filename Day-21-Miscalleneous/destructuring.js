1f8811d03d5e5200eafdfc08cb48f3d62aec48f9
Om Raja <omraja451@gmail.com>
2024-05-05 23:08:51 +0530

    learnt about passig default value in object destructuring

--- Day-21-Miscalleneous/destructuring.js
+++ Day-21-Miscalleneous/destructuring.js
@@ -7,3 +7,20 @@ console.log(Om); // Om
 console.log(Niku); // Nikita
 console.log(Kahishiya); // Kashish
 console.log(others); // [abc, def, ghi]
+
+// destructuring in object
+let obj = {
+    name: "Om Raja",
+    crn: 2215197,
+    urn: 2203743,
+    branch: "CSE",
+    home: "Bhavara",
+    college: "Gndec",
+    // isHosteler: false 
+};
+
+let {name, branch, ghar} =obj;
+console.log(name, branch, ghar); // Om Raja, CSE, undefined
+
+let {home: Myghar, college: chutiyapa, isHosteler = true} = obj;
+console.log(Myghar, chutiyapa, isHosteler); // bhavara, gndec, true
