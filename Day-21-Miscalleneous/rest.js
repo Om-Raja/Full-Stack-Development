74bf2e48ae637f3f8ea02505621f8cf8f9d58650
Om Raja <omraja451@gmail.com>
2024-05-05 22:30:36 +0530

    added code to sum up all arguments using reduce method of array

--- Day-21-Miscalleneous/rest.js
+++ Day-21-Miscalleneous/rest.js
@@ -8,14 +8,17 @@ function sum(...args){
 
     console.log(`number of elements in the args is ${args.length}`);
 
+    let result = args.reduce((acc, ele)=> (acc + ele), 0);
+
     // arguments keyword
     console.log(arguments); // it stores all the argument but it is not an array
     console.log(arguments[2]); // you can use index but you can not apply any array mehtod on arguments
 
     // arguments.pop(); // error because arguments is not an array
+    return result;
 
-    
 
 }
 
-let ans = sum(1, 2, 3);
+let ans = sum(1, 2, 3, 4, 5);
+console.log(`sum is ${ans}`);
