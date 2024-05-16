86103af7458e4a1f01f0ee58236a2708780ab94d
Om Raja <omraja451@gmail.com>
2024-05-16 15:21:24 +0530

    learnt that promise can be written outside a function

--- cohort-2/async and sync js/promise.js
+++ cohort-2/async and sync js/promise.js
@@ -14,3 +14,27 @@ function printKaro(data){
 }
 
 readTheFile().then(printKaro);
+
+// promise can be defined outside an function
+let p = new Promise(function(kuchhbhi){
+    fstat.readFile('file.txt', 'utf-8', function(err, data){
+        kuchhbhi(`second promise ${data}`);
+    });
+})
+
+console.log(p);
+
+p.then(printKaro)
+
+console.log(p);
+
+// a promise has three state 
+// 1. pending
+// 2. resolved
+// 3. rejected
+
+p.then(function(data){
+    console.log(data);
+})
+
+
