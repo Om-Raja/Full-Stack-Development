cfda00bd0f92e21d780b0f5c552db23f2b9abab3
Om Raja <omraja451@gmail.com>
2024-07-15 18:38:15 +0530

    used get random dog image API

--- Day-27-Axios/app2.js
+++ Day-27-Axios/app2.js
@@ -1,6 +1,10 @@
 let url = "https://catfact.ninja/fact";
+let dogApi = "https://dog.ceo/api/breeds/image/random";
+
 let p = document.querySelector('p');
 let button = document.querySelector("button");
+let dogImg = document.querySelector("#dogImg");
+let dogImgBtn = document.querySelector("#dogImgBtn");
 
 async function getFact(){
     try{
@@ -12,7 +16,24 @@ async function getFact(){
     }
 }
 
+async function getDogImg(){
+    try{
+        let link = await axios.get(dogApi);
+        console.log(link);
+        dogImg.setAttribute("src", link.data.message);
+
+    }catch(error){
+        console.log("Image not found : ", error);
+    }
+}
+
 button.addEventListener("click", function(){
     getFact();
     console.log("call lag gaya hai getFact ko");
+});
+
+dogImgBtn.addEventListener("click", function(){
+    getDogImg();
 })
+
+console.log("I am the end");
